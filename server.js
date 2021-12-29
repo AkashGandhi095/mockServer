97a458cdac0055673abafdbfc58f93163b0334ac
DESKTOP-UMRVLHB\Akash <gandhiakash8111@gmail.com>
2021-12-29 18:51:26 +0530

    fixed port

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middleware = jsonServer.defaults();
 
-const host = '0.0.0.0'
-const port = process.env.port || 3000;
+const host = "0.0.0.0";
+const port = process.env.PORT || 3000;
 
 server.use(middleware);
 server.use(router);
-server.listen(port , host , function() {
-    console.log('server started...')
+server.listen(port, host, function () {
+  console.log("server started...");
 });
