fe785fc95d6ce01434e51da076373aa9c08fe294
DESKTOP-UMRVLHB\Akash <gandhiakash8111@gmail.com>
2021-12-29 13:47:22 +0530

    code fixes

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-'use strict';
+"use strict";
 
-const jsonServer = require('json-server')
+const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router('db.json')
-const middleware = jsonServer.middleware()
+const router = jsonServer.router("db.json");
+const middleware = jsonServer.defaults();
 const port = process.env.port || 3200;
 
-server.use(middleware)
-server.use(router)
-server.listen(port)
+server.use(middleware);
+server.use(router);
+server.listen(port);
